29be3be6a0206bca1164474dd46b67703e07912f
nomispaz <111981114+nomispaz@users.noreply.github.com>
2023-04-24 10:11:15 +0000

    Update 3_chroot.sh

--- installer/3_chroot.sh
+++ installer/3_chroot.sh
@@ -26,3 +26,18 @@ echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/00cpu-flags
 
 # skipping licenses for now
 
+echo "set locales and time"
+ln -sf ../usr/share/zoneinfo/Europe/Berlin /etc/localtime
+
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "de_DE.UTF-8 UTF-8" >> /etc/locale.gen
+echo "KEYMAP=de-latin1" >> /etc/vconsole.conf
+echo "XMGneo15Arch" >> /etc/hostname
+locale-gen
+
+echo "select locale with eselect locale set xxx"
+eselect locale list
+
+echo "reload the environment"
+env-update && source /etc/profile && export PS1="(chroot) ${PS1}"
