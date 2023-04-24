987577a0e15f4f23ea923eb3470db7429535f412
nomispaz <111981114+nomispaz@users.noreply.github.com>
2023-04-24 11:44:39 +0000

    Update 3_chroot.sh

--- installer/3_chroot.sh
+++ installer/3_chroot.sh
@@ -95,3 +95,5 @@ echo 'GRUB_PLATFORMS="efi-64"' >> /etc/portage/make.conf
 emerge --ask sys-boot/grub
 grub-install --target=x86_64-efi --efi-directory=/boot/efi
 grub-mkconfig -o /boot/grub/grub.cfg
+
+#skip efi bootmgr for now
