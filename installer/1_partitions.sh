499ab58af17a26040cc613088260b19d88d94a9a
nomispaz <111981114+nomispaz@users.noreply.github.com>
2023-04-25 14:09:40 +0200

    Update 1_partitions.sh

--- installer/1_partitions.sh
+++ installer/1_partitions.sh
@@ -52,4 +52,4 @@ mount --mkdir /dev/$efiDrive /mnt/gentoo/boot/efi
 swapon /dev/$swapDrive
 
 cd /mnt/gentoo
-echo "next step: get wget stage_3 tarball. Then run 2_install.sh"
+echo "next step: get wget stage_3 tarball. Then run /home/gentoo/gentoo/install/2_preparations.sh. Never leave /mnt/gentoo!"
