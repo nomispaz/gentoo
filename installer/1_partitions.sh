fd374758c8670b75382041e64e575425cf710b56
nomispaz <111981114+nomispaz@users.noreply.github.com>
2023-06-18 11:44:32 +0200

    Update 1_partitions.sh

--- installer/1_partitions.sh
+++ installer/1_partitions.sh
@@ -42,5 +42,5 @@ echo "mount root partition"
 mount --mkdir -o noatime,compress=zstd,subvol=root /dev/$rootDrive /mnt/gentoo
 
 cd /mnt/gentoo/
-wget "https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds/20230611T170207Z/stage3-x32-systemd-mergedusr-20230611T170207Z.tar.xz"
+wget "https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds/20230611T170207Z/stage3-amd64-desktop-systemd-20230611T170207Z.tar.xz"
 echo "Run /home/gentoo/gentoo/install/2_preparations.sh. Never leave /mnt/gentoo!"
