49e3a446e9430766a9b24eb2c2986d40f6db9731
nomispaz <111981114+nomispaz@users.noreply.github.com>
2023-08-20 21:59:49 +0200

    Update 5_afterinstall.sh

--- installer/5_afterinstall.sh
+++ installer/5_afterinstall.sh
@@ -1,1 +1,6 @@
+#enable pipewire for user
+systemctl --user --now disable pulseaudio.service pulseaudio.socket
+systemctl --user --now enable pipewire.socket pipewire-pulse.socket
+systemctl --user --now disable pipewire-media-session.service
+systemctl --user --force enable wireplumber.service
 
