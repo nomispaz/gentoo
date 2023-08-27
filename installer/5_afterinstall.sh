a6e0d48f414fb4b547f2f2d0cc6763332e4e66e1
nomispaz <111981114+nomispaz@users.noreply.github.com>
2023-08-27 16:08:49 +0200

    Update 5_afterinstall.sh

--- installer/5_afterinstall.sh
+++ installer/5_afterinstall.sh
@@ -4,3 +4,22 @@ systemctl --user --now enable pipewire.socket pipewire-pulse.socket
 systemctl --user --now disable pipewire-media-session.service
 systemctl --user --force enable wireplumber.service
 
+#enable nvidia-powerd
+sudo systemctl enable nvidia-powerd.service
+sudo systemctl start nvidia-powerd.service
+
+sudo emerge --ask \
+app-crypt/veracrypt \
+mail-client/thunderbird-bin \
+media-fonts/fontawesome \
+net-im/discord \
+gui-apps/grim \
+gui-apps/slurp \
+gui-wm/gamescope \
+kde-apps/kio-extras \
+net-misc/yt-dlp
+
+#flathub
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub com.usebottles.bottles
+flatpak install flathub com.github.tchx84.Flatseal
