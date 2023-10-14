918783b78aed75ac59432a50cd83a88b216cc00a
nomispaz <111981114+nomispaz@users.noreply.github.com>
2023-10-14 20:38:19 +0200

    Delete home directory

--- home/.config/qtile/autostart.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-### AUTOSTART PROGRAMS ###
-wl-clipboard-history -t &
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=qtile &
-/usr/libexec/polkit-gnome-authentication-agent-1 &
-nm-applet --indicator &
